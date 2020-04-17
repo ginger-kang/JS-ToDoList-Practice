86104fa06790221a123c568eec7e2bb668872fff
ginger-kang <kdhoon07@gmail.com>
2020-04-18 02:14:49 +0900

    lastmodified

--- index.js
+++ index.js
@@ -12,14 +12,18 @@ function addElement(inputText) {
     var listElement = document.createElement('li');
     var deleteBtn = document.createElement('button');
     var textSpan = document.createElement('span');
+    var lastModified = document.createElement('span');
 
     deleteBtn.innerText = '❌';
     deleteBtn.className = "delButton";
     deleteBtn.addEventListener("click", delElement);
     textSpan.innerText = inputText;
+    lastModified.innerText = document.lastModified;
+    lastModified.className = "createTime";
 
     listElement.appendChild(textSpan);
     listElement.appendChild(deleteBtn);
+    listElement.appendChild(lastModified);
 
     toDoList.appendChild(listElement);
 }
